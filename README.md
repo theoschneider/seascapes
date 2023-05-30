14f96a36839c128a65d91be6ac46faf645f49fa7
Théo <theo.schnd@gmail.com>
2023-05-30 13:38:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Seascapes
+
+This repository is meant to provide the necessary scripts and data to reproduce the figures shown in the manuscript on a local computer.
+
+The experiments are meant to run on Linux/Unix/macOS operating systems.
+
+If problems and/or questions are encountered, feel free to [open issues](https://github.com/ThibaultLatrille/Seascapes/issues).
+
+## 0. Local copy
+Clone the repository and `cd` to the dir.
+```
+git clone https://github.com/theoschneider/seascapes
+cd seascapes
+```
+
+## 1. Installation
+
+### General dependencies
+
+Install python3 packages
+```
+sudo apt install -qq -y python3-dev python3-pip snakemake
+pip3 install --user scipy numpy matplotlib pandas statsmodels
+```
+
+## 2. Run global analysis
+
+
+In root folder run `snakemake`:
+```
+snakemake -k -j 8
+```
+
+## 3. Add features
+You made modifications to one of the python script, a notebook, this README.md, or you added new features.
+You wish this work benefits to all (futur) users of this repository?
+Please, feel free to open a [pull-request](https://github.com/ThibaultLatrille/Seascapes/pulls)
+
+## Licence
+
+The MIT License (MIT)
+
+Copyright (c) 2019 Thibault Latrille
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
---
+++ scripts/libraries.py
@@ -0,0 +1,82 @@
+import os
+import gzip
+import shutil
+from collections import defaultdict
+import pandas as pd
+
+# Define the codon table
+codontable = defaultdict(lambda: "-")
+codontable.update({
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': 'X', 'TAG': 'X',
+    'TGC': 'C', 'TGT': 'C', 'TGA': 'X', 'TGG': 'W'})
+
+
+def open_fasta(path, filter_species=None):
+    if filter_species is None:
+        filter_species = []
+    outfile = {}
+    ali_file = gzip.open(path, 'rt') if path.endswith(".gz") else open(path, 'r')
+    for seq_id in ali_file:
+        sp_id = seq_id.replace('>', '').strip()
+        seq = ali_file.readline().strip()
+        if (len(filter_species) > 0) and (sp_id not in filter_species):
+            continue
+        outfile[sp_id] = seq
+    print(f"Read {len(outfile)} sequences from {path}")
+    return outfile
+
+
+def write_fasta(dico_fasta, output):
+    outfile = gzip.open(output, 'wt') if output.endswith(".gz") else open(output, 'w')
+    outfile.write("\n".join([f">{seq_id}\n{seq}" for seq_id, seq in dico_fasta.items()]))
+    outfile.close()
+    print(f"Written {len(dico_fasta)} sequences to {output}")
+
+
+def write_ali(dico_fasta, output):
+    outfile = gzip.open(output, 'wt') if output.endswith(".gz") else open(output, 'w')
+    # Assert that all sequences have the same length
+    set_seq_len = set([len(seq) for seq in dico_fasta.values()])
+    assert len(set_seq_len) == 1, "All sequences must have the same length"
+    seq_len = set_seq_len.pop()
+
+    # Open the output file
+    outfile = open(args.output, 'w')
+    # Write the header (the number of sequences and the sequence length)
+    outfile.write(f"{len(dico_fasta)} {seq_len}\n")
+    # Write the names and sequences
+    outfile.write("\n".join([f"{name} {seq}" for name, seq in dico_fasta.items()]))
+    outfile.close()
+    print(f"Written {len(dico_fasta)} sequences to {output}")
+
+
+def zip_file(input_path, output_path=None):
+    if not os.path.isfile(input_path):
+        return
+    if output_path is None:
+        output_path = input_path + ".gz"
+    with open(input_path, 'rb') as f_in:
+        with gzip.open(output_path, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove(input_path)
+    
+def translate_cds(seq):
+    return "".join([codontable[seq[i * 3:i * 3 + 3]] for i in range(len(seq) // 3)])
+    
+def read_profiles(path):
+    return pd.read_csv(path, sep="\t", skiprows=1, header=None, names="site,A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y".split(","))
+
