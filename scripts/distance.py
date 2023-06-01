ec1781572da0ffa796f3ac825fbb36025971c2dc
Théo <theo.schnd@gmail.com>
2023-06-01 16:56:45 +0200

    script distance.py

---
+++ scripts/distance.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main(file1: str, file2: str, output_dir: str):
+    df1 = pd.read_csv(file1, sep="\t", header=0)
+    df2 = pd.read_csv(file2, sep="\t", header=0)
+
+    kl1 = np.sum(df1 * np.log(df1 / df2), axis=1)
+    kl2 = np.sum(df2 * np.log(df2 / df1), axis=1)
+
+    distance = (kl1 + kl2)/2
+
+    distance.to_csv(output_dir+"/distance", sep="\t")
+    with open(output_dir+"/distance", 'w') as outfile:
+        outfile.write(f"{distance}\n")
+
+    fig = distance.plot()
+    fig.savefig(output_dir+"distance.pdf")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--file1', required=True, type=str, dest="file1", help="First .siteprofiles file path")
+    parser.add_argument('--file2', required=True, type=str, dest="file2", help="Second .siteprofiles file path")
+    parser.add_argument('--output', required=True, type=str, dest="output_dir", help="Output file path")
+    args = parser.parse_args()
+    main(args)
--- scripts/read.sh
+++ scripts/read.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 # Set directory
 folder=~/THÉO/seascapes
 
-readmutselomega --every 1 --until 2000 --burnin 1000 --ss run_mutsel_TSPAN6
+# Read the 2 mutsel previously ran
+${folder}/utils/bayescode/bin/readmutselomega --every 1 --until 2000 --burnin 1000 --ss ${folder}/processed/mutsel_TSPAN6_1
+${folder}/utils/bayescode/bin/readmutselomega --every 1 --until 2000 --burnin 1000 --ss ${folder}/processed/mutsel_TSPAN6_2
 
 # moyenne = premiere ligne commentées
 # toute les valeurs en csv en dessous
