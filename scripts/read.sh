8c16412913121bf9d76825c770105ea509904ffa
Théo <theo.schnd@gmail.com>
2023-06-07 16:32:43 +0200

    updated read.sh, removed /processed

--- scripts/read.sh
+++ scripts/read.sh
@@ -2,11 +2,23 @@
 # Set directory
 folder=~/THÉO/seascapes
 
-# Read the 2 mutsel previously ran
-${folder}/utils/bayescode/bin/readmutselomega --every 1 --until 2000 --burnin 1000 --ss ${folder}/processed/mutsel_TSPAN6_1
-${folder}/utils/bayescode/bin/readmutselomega --every 1 --until 2000 --burnin 1000 --ss ${folder}/processed/mutsel_TSPAN6_2
+until=2000
+delta_t=100
 
-# Compute the distance between the 2 runs
-python3 ${folder}/scripts/distance.py --file1 ${folder}/processed/mutsel_TSPAN6_1.siteprofiles \
-                                      --file2 ${folder}/processed/mutsel_TSPAN6_2.siteprofiles \
-                                      --output ${folder}/results/
+for burnin in seq 0 ${until} ${delta_t}
+do
+  ${folder}/utils/bayescode/bin/readmutselomega --every 1 \
+                                                --burnin ${burnin} \
+                                                --until ${burnin}+${delta_t} \
+                                                --ss ${folder}/processed/mutsel_TSPAN6_1
+
+  ${folder}/utils/bayescode/bin/readmutselomega --every 1 \
+                                                --burnin ${burnin} \
+                                                --until ${burnin}+${delta_t} \
+                                                --ss ${folder}/processed/mutsel_TSPAN6_2
+
+  # Compute the distance between the 2 runs
+  python3 ${folder}/scripts/distance.py --file1 ${folder}/processed/mutsel_TSPAN6_1.siteprofiles \
+                                        --file2 ${folder}/processed/mutsel_TSPAN6_2.siteprofiles \
+                                        --output ${folder}/results/
+done
