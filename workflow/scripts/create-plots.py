ef3030bc576a9209ca5340ab6f588235e9d5948d
Théo <theo.schnd@gmail.com>
2023-08-10 11:23:11 +0200

    changed create-plots.py to accept a list of genes to process

--- workflow/scripts/create-plots.py
+++ workflow/scripts/create-plots.py
@@ -5,18 +5,36 @@ import matplotlib.pyplot as plt
 # Set the source directory
 SOURCE_DIR = "/Users/theo/THÉO/seascapes"
 
-# Define the range (FROM and TO) and the flag to process all folders (ALL)
+# Define the range (FROM and TO), the flag to process all folders (ALL) and the names of the genes (if needed)
 FROM = 1
 TO = 5
 ALL = False
+GENES = ["ENSG00000204120_GIGYF2",
+         "ENSG00000150401_DCUN1D2",
+         "ENSG00000151364_KCTD14",
+         "ENSG00000110583_NAA40",
+         "ENSG00000204536_CCHCR1",
+         "ENSG00000198604_BAZ1A",
+         "ENSG00000154146_NRGN",
+         "ENSG00000108582_CPD",
+         "ENSG00000163349_HIPK1",
+         "ENSG00000111540_RAB5B",
+         "ENSG00000147432_CHRNB3",
+         "ENSG00000011198_ABHD5"]
 
 # Check if ALL is true, then adjust FROM and TO to process all folders
 if ALL:
     FROM = 1
     TO = len(os.listdir(SOURCE_DIR))
 
+# Check if some genes have been specified
+if GENES:
+    genes_list = GENES
+else:
+    genes_list = os.listdir(os.path.join(SOURCE_DIR, "processed"))[FROM:TO + 1]
+
 # Iterate over the contents of SOURCE_DIR and process folders
-for folder_name in os.listdir(os.path.join(SOURCE_DIR, "processed"))[FROM:TO + 1]:
+for folder_name in genes_list:
     folder_path = os.path.join(SOURCE_DIR, "processed", folder_name)
     print(f"Processing folder: {folder_path}")
     # In every folder, open distance.tsv
