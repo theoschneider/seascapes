2b123e99783ef9b35fdc8c918e0cb9461c09ffed
Théo <theo.schnd@gmail.com>
2023-08-16 11:25:19 +0200

    changed create-plots.py to plot the distance vs omega, distance vs omega0 and distance vs omegaA in addition to the distance per site

--- workflow/scripts/create-plots.py
+++ workflow/scripts/create-plots.py
@@ -21,6 +21,7 @@ GENES = ["ENSG00000204120_GIGYF2",
          "ENSG00000111540_RAB5B",
          "ENSG00000147432_CHRNB3",
          "ENSG00000011198_ABHD5"]
+CLADE = "Euarchontoglires"
 
 # Check if ALL is true, then adjust FROM and TO to process all folders
 if ALL:
@@ -35,13 +36,15 @@ else:
 
 # Iterate over the contents of SOURCE_DIR and process folders
 for folder_name in genes_list:
+    # For every folder
     folder_path = os.path.join(SOURCE_DIR, "processed", folder_name)
     print(f"Processing folder: {folder_path}")
-    # In every folder, open distance.tsv
-    df = pd.read_csv(os.path.join(folder_path, "distance.tsv"), sep="\t", header=0, index_col=None)
+
+    # Open distance.tsv
+    distance = pd.read_csv(os.path.join(folder_path, "distance.tsv"), sep="\t", header=0, index_col=None)
     # Plot the distance
     plt.figure(figsize=(12, 7))
-    plt.plot(df.iloc[:, 0], df.iloc[:, 1])
+    plt.plot(distance.iloc[:, 0], distance.iloc[:, 1])
     plt.xlabel("Position")
     plt.ylabel("Distance")
     plt.title("Jensen-Shannon divergence between two subsets, compared to base distance for each subset")
@@ -49,4 +52,36 @@ for folder_name in genes_list:
     # Create directory and save the plot
     os.makedirs(os.path.join(SOURCE_DIR, "results", folder_name), exist_ok=True)
     plt.savefig(SOURCE_DIR + "/results/" + folder_name + "/distance.pdf")
+
+    # Open estimated omega and omega0, compute omegaA
+    omega = pd.read_csv(SOURCE_DIR + "/processed/" + folder_name + "/" + CLADE + ".Whole.omega.ci0.025.tsv", sep="\t", header=0, index_col=0)
+    omega0 = pd.read_csv(SOURCE_DIR + "/processed/" + folder_name + "/" + CLADE + ".Whole.omega_0.ci0.025.tsv", sep="\t", header=0, index_col=0)
+    omegaA = omega - omega0
+
+    # Plot distance vs omega and save it
+    plt.figure(figsize=(7, 7))
+    plt.scatter(omega.iloc[1:, 1], distance.iloc[:, 1], s=2)
+    plt.xlabel("Omega")
+    plt.ylabel("Distance")
+    plt.title("Distance vs omega")
+    plt.tight_layout()
+    plt.savefig(SOURCE_DIR + "/results/" + folder_name + "/distance-vs-omega.pdf")
+
+    # Plot distance vs omega0 and save it
+    plt.figure(figsize=(7, 7))
+    plt.scatter(omega0.iloc[1:, 1], distance.iloc[:, 1], s=2)
+    plt.xlabel("Omega 0")
+    plt.ylabel("Distance")
+    plt.title("Distance vs omega 0")
+    plt.tight_layout()
+    plt.savefig(SOURCE_DIR + "/results/" + folder_name + "/distance-vs-omega0.pdf")
+
+    # Plot distance vs omegaA and save it
+    plt.figure(figsize=(7, 7))
+    plt.scatter(omegaA.iloc[1:, 1], distance.iloc[:, 1], s=2)
+    plt.xlabel("OmegaA")
+    plt.ylabel("Distance")
+    plt.title("Distance vs omegaA (omega - omega0)")
+    plt.tight_layout()
+    plt.savefig(SOURCE_DIR + "/results/" + folder_name + "/distance-vs-omegaA.pdf")
 
