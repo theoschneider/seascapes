a390b7dde1112dc2edd3c83ce21772b0fdcbcf7b
Théo <theo.schnd@gmail.com>
2023-09-21 17:13:00 +0200

    outside_CI.py now works

--- workflow/scripts/outside_CI.py
+++ workflow/scripts/outside_CI.py
@@ -1,17 +1,24 @@
 import numpy as np
 import pandas as pd
-from libraries import getconfint, open_fasta
+from libraries import getconfint, open_ali, write_fasta
 
 folder = "/Users/theo/THÉO/seascapes/"
 
 # Read the data
-fasta = open_fasta(folder + "data/omm_NT_fasta.v10c_116/ENSG00000150401_DCUN1D2_NT.fasta")
+sequences = open_ali(folder + "processed/ENSG00000150401_DCUN1D2/Euarchontoglires.Whole.phy")
 distance = pd.read_csv(folder + "processed/ENSG00000150401_DCUN1D2/distance.tsv", sep="\t", header=0, index_col=None)
 omega = pd.read_csv(folder + "processed/ENSG00000150401_DCUN1D2/Euarchontoglires.Whole.omega.ci0.025.tsv", sep="\t", header=0, index_col=0)
 
 # Run the linear regression
 lower_ci, upper_ci = getconfint(x=omega.iloc[1:, 1].tolist(), y=distance.iloc[:, 1].tolist(), alpha=0.05)
 
-for position in range(len(distance.iloc[:, 1])):
-    print(distance.iloc[position, 1], omega.iloc[position + 1, 1], lower_ci[position], upper_ci[position])
+sequences["conf_filter"] = ""
 
+for position in range(len(distance.iloc[:, 1])):
+
+    if (distance.iloc[position, 1] < lower_ci[position]) or (distance.iloc[position, 1] > upper_ci[position]):
+        sequences["conf_filter"] += "AAA"
+    else:
+        sequences["conf_filter"] += "TTT"
+
+write_fasta(sequences, folder + "processed/ENSG00000150401_DCUN1D2/Euarchontoglires.Whole.conf_filter.fasta")
