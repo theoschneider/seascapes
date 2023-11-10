de67a2480d76c41325e03edeaa84dbb8eba18bef
Théo <theo.schnd@gmail.com>
2023-11-10 14:59:28 +0100

    corrected python version

--- workflow/scripts/run_snakemake.sh
+++ workflow/scripts/run_snakemake.sh
@@ -6,9 +6,8 @@
 #SBATCH --partition cpu
 #SBATCH --mem 2G
 #SBATCH --time 23:59:00
-#SBATCH --account tschnei6
 
 # Load the required software: e.g.
-module load gcc python
+module load gcc/10.4.0 python/3.6.13
 
-snakemake -k -j 100 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
+snakemake -k -s /work/FAC/FBM/DBC/nsalamin/default/tschnei6/seascapes/workflow/Snakefile -j 100 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
