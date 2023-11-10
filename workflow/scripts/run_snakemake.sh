87fc8a66d08650a20409e8da0417b371f271596a
Théo <theo.schnd@gmail.com>
2023-11-10 15:21:49 +0100

    removed module load

--- workflow/scripts/run_snakemake.sh
+++ workflow/scripts/run_snakemake.sh
@@ -7,7 +7,5 @@
 #SBATCH --mem 2G
 #SBATCH --time 23:59:00
 
-# Load the required software: e.g.
-module load gcc/10.4.0 python/3.6.13
 
 snakemake -k -s /work/FAC/FBM/DBC/nsalamin/default/tschnei6/seascapes/workflow/Snakefile -j 100 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
