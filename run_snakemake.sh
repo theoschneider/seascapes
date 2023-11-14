d51ac11371bbef9d0cfbd7df23a395cb9bb48f1b
Théo <theo.schnd@gmail.com>
2023-11-14 10:42:48 +0100

    added genes and changed paramters for optimal cluster execution

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --partition cpu
-#SBATCH --mem 2G
-#SBATCH --time 23:59:00
+#SBATCH --mem 6G
+#SBATCH --time 2-23:59:59
 
 
-snakemake -k -s /work/FAC/FBM/DBC/nsalamin/default/tschnei6/seascapes/workflow/Snakefile -j 100 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
+snakemake -k -s /work/FAC/FBM/DBC/nsalamin/default/tschnei6/seascapes/workflow/Snakefile -j 4096 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} --time={params.time}"
