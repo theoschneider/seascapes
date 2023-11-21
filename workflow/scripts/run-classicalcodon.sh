f1c42ac6158bac13099988317049719de5d3d9ac
Théo <theo.schnd@gmail.com>
2023-11-21 10:47:00 +0100

    removed old files

--- workflow/scripts/run-classicalcodon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set directory
-folder=~/THÉO/seascapes
-
-${folder}/utils/bayescode/bin/mutselomega --freeomega --omegancat 30 --flatfitness \
-                                          -a ${folder}/processed/TSPAN6_NT.phy \
-                                          -t ${folder}/data/omm_RooTree.v10b_116/ENSG00000000003_TSPAN6_NT.rootree \
-                                          -u 2000 ${folder}/classical_mutsel_TSPAN6
