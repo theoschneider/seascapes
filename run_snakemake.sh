3c68747f954926635b935db839530f446e26832b
Théo <theo.schnd@gmail.com>
2023-12-06 11:51:39 +0100

    prepared scripts to use adaptive genes in the simulations

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -8,4 +8,4 @@
 #SBATCH --time 2-23:59:59
 
 
-snakemake -k -s /work/FAC/FBM/DBC/nsalamin/default/tschnei6/seascapes/workflow/Snakefile -j 4096 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} --time={params.time}"
+snakemake -k -s /scratch/tschnei6/seascapes/workflow/SnakeSimu -j 10000 --cluster "sbatch -J {params.name} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} --time={params.time}"
